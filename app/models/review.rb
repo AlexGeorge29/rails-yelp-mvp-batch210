b4097936d2304e036fb11d2bf30f3a0311072488
alex george <alexandreantoinegeorge@gmail.com>
2019-02-12 18:48:41 -0500

    all back end ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  rating_list = (0..5).to_a
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: rating_list }, numericality: { only_integer: true }
 end
